9c8c2e684f628f214b4e771d3eb2bf6c082c3617
LimongiVirgil <limongi.virgil@gmail.com>
2021-07-18 19:10:01 +0200

    format date/time for issuedate component and format phrasing for issueMessage component

--- src/components/molecules/issueInformation/issueInformation.jsx
+++ src/components/molecules/issueInformation/issueInformation.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import IssueMessage from '../../atoms/issueMessage/issueMessage'
 import IssueDate from '../../atoms/issueDate/issueDate'
 
 const IssueInformation = () => {
+	const [issuesData, setIssuesData] = useState([])
+
+	useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/dashboard/futureEvent/1')
+      .then(response => response.json())
+			.then(result => console.log(result));
+      //.then(result => setIssuesData(result));
+  })
+
 	return (
 		<div>
-			<IssueDate date="Ven 16 juil." time="8h30"/>
-			<IssueMessage text="Intervention salle B008 par S. Baudu, pour consultation suite à un taux d’humidité élevé"/>
+			<IssueDate date="2021-07-08 16:00:00.000000"/>
+			<IssueMessage incidentType="defective_air_conditioning" company="Solar" classroomName="110" classroomZone="B"/>
 		</div>
 	)
 }
