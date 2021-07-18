00e7427efa22312f9d48e2080b9d89ade552c8d3
Pierre-Alain8 <piaiackerman@gmail.com>
2021-07-18 21:37:58 +0200

    style(dropdownTypesIncidents): update name class

--- src/components/organisms/DropDownIncidentsTypes/DropdownIncidentsTypes.jsx
+++ src/components/organisms/DropDownIncidentsTypes/DropdownIncidentsTypes.jsx
@@ -3,22 +3,55 @@ import ListIncidentsTypes from '../../molecules/listIncidentsTypes/ListIncidents
 import { Menu, Transition } from '@headlessui/react'
 
 const DropDownIncidentsTypes = () => {
+  const mockHeatLeakIncidents = [
+    {
+      incident_id: 3,
+      incident_title: "incident salle B106",
+      incident_date:"2021-07-07 08:05:06",
+      incident_type: "heat_leak",
+      incident_status: "finish",
+      classroom_name: "106",
+      classroom_floor: 1,
+      classroom_zone: "B",
+    },
+    {
+      incident_id: 22,
+      incident_title: "incident salle A204",
+      incident_date:"2020-07-07 15:06:56",
+      incident_type: "heat_leak",
+      incident_status: "assign",
+      classroom_name: "204",
+      classroom_floor: 2,
+      classroom_zone: "A",
+    },
+    {
+      incident_id: 23,
+      incident_title: "incident salle A205",
+      incident_date:"2020-07-07 13:06:56",
+      incident_type: "heat_leak",
+      incident_status: "finish",
+      classroom_name: "205",
+      classroom_floor: 2,
+      classroom_zone: "A",
+    },
+
+  ]
 return (
-    <Menu as="div" className="dropdownIncidents relative inline-block text-left">
+    <Menu as="div" className="dropdown-incidents relative inline-block text-left">
       {({ open }) => (
         <>
-          <div className="dropdownHeader">
+          <div className="dropdown-header">
             <div>
               <h3>Fuite de châleur</h3>
-              <p>13 incidents</p>
+              <p>{mockHeatLeakIncidents.length} incidents</p>
             </div>
-            <Menu.Button className="dropdownButton">
-              Ouvrir le volet
+            <Menu.Button className="button-dropdown">
+              { open ? "Ouvrir le volet" : "Fermer le volet"}
             </Menu.Button>
           </div>
 
-          <div className="detailedInformations">
-            <span> Date de relevé </span> 
+          <div className="detailed-informations">
+            <span className="statement-date"> Date de relevé </span> 
             <span> Localisation </span> 
             <span> Statut </span>
           </div>
@@ -34,7 +67,14 @@ return (
               leaveFrom="transform opacity-100 scale-100"
               leaveTo="transform opacity-0 scale-95"
             > 
-              < ListIncidentsTypes />
+            {
+              mockHeatLeakIncidents.map((incidentItem)=>{
+                return(
+                  < ListIncidentsTypes incidents={incidentItem} key={incidentItem.incident_id} />
+                )
+              })
+            }
+            
             </Transition.Child>
           </Transition>
         </>
