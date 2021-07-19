a55cbfd6f9d5f02e60593ec697b270b0e2150fba
TristanLemire <tristan.lemire.sio@gmail.com>
2021-07-19 19:46:39 +0200

    Add nb incients

--- src/components/organisms/DropDownIncidentsTypes/DropdownIncidentsTypes.jsx
+++ src/components/organisms/DropDownIncidentsTypes/DropdownIncidentsTypes.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect ,useState  } from 'react'
-import ListIncidentsTypes from '../../molecules/listIncidentsTypes/ListIncidentsTypes';
-import { Menu, Transition } from '@headlessui/react'
+import React, { useEffect, useState } from "react";
+import ListIncidentsTypes from "../../molecules/listIncidentsTypes/ListIncidentsTypes";
+import { Menu, Transition } from "@headlessui/react";
 
 const incidentTitle = {
-  high_humidity : 'Haute humidité',
-  heat_leak: 'Fuite de châleur',
-  defective_air_conditioning : 'Air conditionnée'
-}
+  high_humidity: "Haute humidité",
+  heat_leak: "Fuite de châleur",
+  defective_air_conditioning: "Air conditionnée",
+};
 
-const DropDownIncidentsTypes = ({incidents, type}) => {
+const DropDownIncidentsTypes = ({ incidents, type }) => {
   // const [incidentsTypeData, setIncidentsTypeData ] = useState([])
   // const SENSOR_TYPE = [
   //   'high_humidity',
@@ -28,48 +28,57 @@ const DropDownIncidentsTypes = ({incidents, type}) => {
   //     console.log(err)
   //   }
   // }
-  // console.log(incidentsTypeData)
 
-return (
-    <Menu as="div" className="dropdown-incidents relative inline-block text-left">
+  console.log("MERDDDEEE");
+  console.log(incidents && incidents[type]);
+
+  return (
+    <>
+      <Menu
+        as="div"
+        className="dropdown-incidents relative inline-block text-left"
+      >
         {({ open }) => (
           <>
-          <div className="dropdown-header">
-            <div>
-              {
-                console.log(incidents)
-              }
-              <h3>{incidentTitle[type]}</h3>
-              {/* <p>{incidents[type].length}</p> */}
+            <div className="dropdown-header">
+              <div>
+                <h3>{incidentTitle[type]}</h3>
+                <p>
+                  {incidents[type] &&
+                    `${
+                      incidents[type].length > 1
+                        ? `${incidents[type].length} incidents`
+                        : `${incidents[type].length} incident`
+                    }`}
+                </p>
+              </div>
+              <Menu.Button className="button-dropdown">
+                {open ? "Fermer le volet" : "Ouvrir le volet"}
+              </Menu.Button>
             </div>
-            <Menu.Button className="button-dropdown">
-              { open ? "Fermer le volet" : "Ouvrir le volet" }
-            </Menu.Button>
-          </div>
-          <div className="detailed-informations">
-            <span className="statement-date"> Date de relevé </span> 
-            <span> Localisation </span> 
-            <span> Statut </span>
-          </div>
-      
-          <Transition
-            show={open}
-          >
-            <Transition.Child
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            > 
-              < ListIncidentsTypes />
-            </Transition.Child>
-          </Transition>
-        </>
+            <div className="detailed-informations">
+              <span className="statement-date"> Date de relevé </span>
+              <span> Localisation </span>
+              <span> Statut </span>
+            </div>
+
+            <Transition show={open}>
+              <Transition.Child
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <ListIncidentsTypes />
+              </Transition.Child>
+            </Transition>
+          </>
         )}
-    </Menu>
-)}
+      </Menu>
+    </>
+  );
+};
 
 export default DropDownIncidentsTypes;
-
