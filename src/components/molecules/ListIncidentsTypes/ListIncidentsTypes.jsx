371d31f7f53b9e153b380b386939570f14e03983
Pierre-Alain8 <piaiackerman@gmail.com>
2021-07-20 17:32:35 +0200

    style(ListIncidentsType): change import issueDate

--- src/components/molecules/ListIncidentsTypes/ListIncidentsTypes.jsx
+++ src/components/molecules/ListIncidentsTypes/ListIncidentsTypes.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Menu } from '@headlessui/react'
 import TagsIncidents from './../../atoms/tagsIncidents/TagsIncidents'
 import LocalisationIncidents from './../../atoms/localisationIncidents/LocalisationIncidents'
-import IssueDate from './../../atoms/issueDate/IssueDate'
+import issueDate from './../../atoms/issueDate/IssueDate'
 
 const tagTitle = {
   assign: 'Intervention programmée',
@@ -22,7 +22,7 @@ return(
   {incidents[type].map((item, key) => (
     <Menu.Item key={key}>
         <div className="incidents">
-          <IssueDate size="small" date={item.incident_date} />
+          <issueDate size="small" date={item.incident_date} />
           <LocalisationIncidents 
             cssClass="localisation-incidents" 
             text={`${item.classroom_zone} + ${item.classroom_floor}`} 
