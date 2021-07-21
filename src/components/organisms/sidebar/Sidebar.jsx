46a5af55ab4e96383a8673794c03deacca70f496
Thomasevano <thomas-evano@hotmail.fr>
2021-07-21 20:16:08 +0200

    Add sidebar

---
+++ src/components/organisms/sidebar/Sidebar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="school-container">
+        <p className="subtitle">Etablissement</p>
+        <p className="school-title">ESGCI</p>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/home">Tableau de bord</NavLink>
+          </li>
+          <li>
+            <NavLink to="/live-data">Surveillance des capteurs</NavLink>
+          </li>
+          <li>
+            <NavLink to="/agenda">Agenda</NavLink>
+          </li>
+          <li>
+            <NavLink to="/settings"> Paramétres</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Sidebar;
--- src/components/pages/Routes.jsx
+++ src/components/pages/Routes.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Sidebar from '../organisms/sidebar/Sidebar';
 import Home from './Home';
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-    </Switch>
+    <Fragment>
+      <Sidebar/>
+      <Switch>
+        <Route path="/home" exact component={Home} />
+      </Switch>
+  </Fragment>
   );
 }
 
