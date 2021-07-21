a7acef1fc2da8caed1210e52bab5f4fd1e110897
ETombuyses <e.tombuyses@gmail.com>
2021-07-21 12:44:59 +0200

    [add] add calendar events list

--- src/components/organisms/commingIssue/commingIssue.jsx
+++ src/components/organisms/commingIssue/commingIssue.jsx
@@ -1,13 +1,23 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Title from "../../atoms/title/title"
 import IssueInformation from "../../molecules/issueInformation/issueInformation"
 import Card from "../../templates/card"
 
 const CommingIssue = () => {
+	const [issuesData, setIssuesData] = useState(false)
+
+	useEffect(() => {
+    fetch('https://architech-hetic.herokuapp.com/api/dashboard/futureEvent/1')
+      .then(response => response.json())
+      .then(result => setIssuesData(result));
+  }, [])
+
 	return (
-		<Card className="commingIssue">
+		<Card>
 			<Title cssClass="card-title">2 évènements à venir</Title>
-			<IssueInformation />
+			{issuesData && issuesData.map((issue, index) => (
+				<IssueInformation issue={issue} key={index}/>
+			))}
 		</Card>
 	);
 };
