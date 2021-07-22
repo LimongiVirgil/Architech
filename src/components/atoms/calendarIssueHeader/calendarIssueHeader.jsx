bfe8e64cab7cdf61dbd939b4b03a908d091894d8
Pierre-Alain8 <piaiackerman@gmail.com>
2021-07-22 11:02:07 +0200

    style(component): add multiple change

--- src/components/atoms/calendarIssueHeader/calendarIssueHeader.jsx
+++ src/components/atoms/calendarIssueHeader/calendarIssueHeader.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const issueHeaderAgenda = ({ cssClass }) => {
   return (
-    <div className={`${cssClass}`}>
-      <p className="hearder-message">Aujourd'hui, <span>15 juillet 2021</span> pas d'évènements prévue </p>
+    <div className={cssClass}>
+      <p className="hearder-message">Aujourd'hui, <span>15 juillet 2021</span> pas d'évènements prévus </p>
       <span className="total-issues"> 6 incidents à examiner</span>
     </div>
   )
