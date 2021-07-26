baf30afb4968683db08e7ab23d726e07c8c7d03a
Pierre-Alain8 <piaiackerman@gmail.com>
2021-07-26 16:42:02 +0200

    [add]: add style effect to inputs

--- src/components/atoms/button/Button.jsx
+++ src/components/atoms/button/Button.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Button ({ text, primary, handleClick }) {
   return (
-    <button className={`button${primary ? ' button--primary' : ''}`} onClick={handleClick}>
+    <button className={
+      `button${primary ? ' button--primary' : ' button--cancel'}`}
+      onClick={handleClick 
+      }
+    >
       {text}
     </button>
   )
