50a04aa0f210dfb8a3c09e600a72fe491af7723d
Pierre-Alain8 <piaiackerman@gmail.com>
2021-07-27 12:06:25 +0200

    [add]: add message for 0 issues in disclosure agenda

--- src/components/molecules/calendarListIssues/calendarListIssues.jsx
+++ src/components/molecules/calendarListIssues/calendarListIssues.jsx
@@ -8,20 +8,22 @@ const calendarListIssues = ({ issues }) => {
       className="list-issues relative focus:outline-none"
     >
       <div className="issues issues--agenda">
-        { issues.map(issue => {
-          const issueDate = new Date(issue.incident_date)
-          return (
-          <div key={issue.incident_id} className="issues__issue">
-            <LocalisationIncidents 
-              cssClass="localisation-incidents" 
-              text={issue.classroom_zone + issue.classroom_name}
-            />
-            <span className="issues-report">
-              {' '} {issueDate.toLocaleDateString('fr-FR', { weekday: 'short',  day: 'numeric', month: 'long',  year: 'numeric'})}
-              {' '} à {issueDate.getHours()}h{issueDate.getMinutes()}
-            </span>
-          </div>
-        )})
+        {issues.length === 0 
+          ? <p className="issues-messages"> Aucun incident à examiner </p>
+          : issues.map(issue => {
+            const issueDate = new Date(issue.incident_date)
+            return (
+            <div key={issue.incident_id} className="issues__issue">
+              <LocalisationIncidents 
+                cssClass="localisation-incidents" 
+                text={issue.classroom_zone + issue.classroom_name}
+              />
+              <span className="issues-report">
+                {' '} {issueDate.toLocaleDateString('fr-FR', { weekday: 'short',  day: 'numeric', month: 'long',  year: 'numeric'})}
+                {' '} à {issueDate.getHours()}h{issueDate.getMinutes()}
+              </span>
+            </div>
+          )})
         }
       </div>
     </Disclosure.Panel>
